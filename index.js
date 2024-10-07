365e46d772f517339e046efd2fde4dc5c9e5cea5
Bhavyadeveshwar <bhavyadeveshwar@gmail.com>
2024-10-07 21:44:16 +0530

    new

---
+++ index.js
@@ -0,0 +1,28 @@
+/*console.log("Hello")
+var newvariable;
+newvariable=8;
+console.log(newvariable);
+var a =7
+var b=23455
+c=a+b
+console.log(c);
+
+
+var bhavya= new Array(3)
+bhavya[0]="Great"
+bhavya[1]="pretty"
+bhavya[2]="djdnkj"
+console.log(bhavya[0])
+
+function grey(person){
+    console.log("Hello " +person+"!")
+    
+}
+grey("bhavya") */
+
+for(i=0; i<3; i++){
+    console.log("b");
+}
+
+document.write("b")
+
